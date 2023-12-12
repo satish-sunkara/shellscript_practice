a953495e1d65aae83d58db6c343885cf3aeadc14
satish-sunkara <satishsunkara01@gmail.com>
2023-12-12 17:41:54 +0530

    dollar variable addition

--- 01_Conversation.sh
+++ 01_Conversation.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "Hi ra kiran, How are you !!!"
-echo "Hi ra satish, I am doing good, How are you...!!!"
-echo "I am also good ra kiran "
+NAME1=$1
+NAME2=$2
+
+echo "Hi ra $NAME1, How are you !!!"
+echo "Hi ra $NAME2, I am doing good, How are you...!!!"
+echo "I am also good ra $NAME1 "
+echo "How is your health ra $NAME2"
+echo "Feeling better ra $NAME1"
