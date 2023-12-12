6478bfc31697ff60b0020aac713af0da0941b0d4
satish-sunkara <satishsunkara01@gmail.com>
2023-12-12 17:29:14 +0530

    Folder Name change

--- 01-hellow-world.sh
+++ 01-hellow-world.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 echo "Welcome to Devops world"
---
+++ 01_Conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hi ra kiran, How are you !!!"
+echo "Hi ra satish, I am doing good, How are you...!!!"
+echo "I am also good ra kiran "
