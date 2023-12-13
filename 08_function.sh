6fd42e6ccea747e4cb58814f56d245fa8b5406d0
satish-sunkara <satishsunkara01@gmail.com>
2023-12-13 19:06:53 +0530

    condition commit

--- 08_function.sh
+++ 08_function.sh
@@ -3,7 +3,7 @@
 USRID=$(id -u)
 
 R="\e[31m"
-G=\e[32m
+G="\e[32m"
 N="\e[0m"
 
 if [ $USRID -ne 0 ]
@@ -15,10 +15,10 @@ fi
 VALIDATE(){
     if [ $? -ne 0 ]
     then
-        echo "Installation $1 $R FAILED $N"
+        echo "Installation $1 ${R} FAILED ${N}"
         exit 1
     else
-        echo "Installation $1 $G SUCCESS $N"
+        echo "Installation $1 ${G} SUCCESS ${N}"
     fi
 }
 
