03b568d12330a4dde5875263dda8c89bdaf4d0d8
satish-sunkara <satishsunkara01@gmail.com>
2023-12-14 00:40:47 +0530

    condition commit

---
+++ 10_for_pkg.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USRID=$(id -u)
+DATE=$(date +%F)
+LOG="/tmp/$DATE.log"
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USRID -ne 0 ]; then
+    echo -e " $Y WARN:$R Please login as root user $N "
+    exit 1
+fi
+
+
+for i in {1..$#}
+    do
+        yum list installed $1 &>> $LOG
+        AVAILABLE(){
+            if [ $1 -ne 0 ]; then
+                echo -e "$2 .... $R FAILED $N "
+            else
+                echo -e "$2 .... $G SUCCESS $N "
+            fi
+        }
+
+
+        VALIDATE(){
+            if [ $1 -ne 0 ]; then
+                yum install $2 -y &>> $LOG
+                AVAILABLE $? "Installation of $2"
+            else
+            echo -e "$Y The package $2 is Already available $N"
+            fi  
+        }
+
+        VALIDATE $? $1
+
+#yum list installed git
+
+#VALIDATE $? "GIT"
