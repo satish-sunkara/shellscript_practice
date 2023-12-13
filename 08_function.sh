f2eac5f356396c0516d11b7c78d6f7433cf3bb7f
satish-sunkara <satishsunkara01@gmail.com>
2023-12-13 19:22:05 +0530

    condition commit

--- 08_function.sh
+++ 08_function.sh
@@ -8,21 +8,21 @@ N="\e[0m"
 
 if [ $USRID -ne 0 ]
 then
-    echo "Please login with root user"
+    echo -e " $R Please login with root user $N"
     exit 1
 fi
 
 VALIDATE(){
     if [ $? -ne 0 ]
     then
-        echo "Installation $1 ${R} FAILED ${N}"
+        echo -e "Installation $1 $R FAILED $N"
         exit 1
     else
-        echo "Installation $1 ${G} SUCCESS ${N}"
+        echo -e "Installation $1 $G SUCCESS $N"
     fi
 }
 
-yum install mysqll-y 
+yum install mysql -y 
 
 VALIDATE "Mysql"
 
