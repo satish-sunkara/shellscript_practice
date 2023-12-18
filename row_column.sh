3ff51032f29ee2a0629f62fcf9ee51a960339005
satish-sunkara <satishsunkara01@gmail.com>
2023-12-19 00:39:28 +0530

    app update1

--- row_column.sh
+++ row_column.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TEXT=$(cat $1)
-a=0
+#a=0
 
 echo " $TEXT "
 
@@ -9,13 +9,16 @@ echo " $TEXT "
 #     echo "$i"
 # done
 
-while IFS= read -r line
-do
-    for i in $line; do
-        echo "$i"
-        a=$a+1
-        if [ $a -eq 2 ]; then
-            exit 1
-        fi
-    done
-done <<< $TEXT
+# while IFS= read -r line
+# do
+#     for i in $line; do
+#         echo "$i"
+#         a=$a+1
+#         if [ $a -eq 2 ]; then
+#             exit 1
+#         fi
+#     done
+# done <<< $TEXT
+
+RESULT= paste -sd /tmp/test.txt
+echo"$  RESULT"
