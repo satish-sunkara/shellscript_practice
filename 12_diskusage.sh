a3e84b059b6e289119ec4c289fcc84e4d7be4b37
satish-sunkara <satishsunkara01@gmail.com>
2023-12-18 16:38:18 +0530

    app update1

--- 12_diskusage.sh
+++ 12_diskusage.sh
@@ -5,11 +5,11 @@ POSITION=$(df -Th | grep -vE 'tempfs|File' | awk -F " " '{print $6F}')
 TRUSHOLD_VALUE=1
 MESSAGE=""
 
-while IFS= read -r $line $line1
+while IFS= read -r $line $line1 $line1
 do
     if [ $line -ge  $TRUSHOLD_VALUE]; then
-        MESSAGE+="High disk usage on $POSITION : $line\n"
+        MESSAGE+="High disk usage on $line1 : $line\n"
     fi
-done <<< $DISK_USAGE 
+done <<< $DISK_USAGE $POSITION 
 
 echo -e "message : $MESSAGE"
