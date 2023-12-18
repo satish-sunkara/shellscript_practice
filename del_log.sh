34ab66d18b0bf28e772ec3b6ae2efd624ba8f778
satish-sunkara <satishsunkara01@gmail.com>
2023-12-18 14:46:03 +0530

    app update1

--- del_log.sh
+++ del_log.sh
@@ -2,6 +2,11 @@
 
 LOGFILE="/tmp/shellscripts-log"
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
 if [ ! -d $LOGFILE ]; then
     echo -e " $R source directory : $LOGFILE doesn't exists $N"
 fi
