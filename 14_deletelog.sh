a336182801a182b349994c6eddfeabdfa924661e
satish-sunkara <satishsunkara01@gmail.com>
2023-12-19 14:58:32 +0530

    logfiles delrtion

--- 14_deletelog.sh
+++ 14_deletelog.sh
@@ -10,7 +10,7 @@ if [ ! -d $LOGFOLDER ]; then
     echo " $R There is no directory $LOGFOLDER is prasent $N "
     exit 1
 fi
-LOGFILE=$( find $LOGFOLDERS -type f -mtime +14 -name "*.log")
+LOGFILE=$( find $LOGFOLDERS -type f -mtime +10 -name "*.log")
 
 while IFS= read -r line
 do
