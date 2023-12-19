fc17434931c627b87d8f8a77856c7eb3a55f0b81
satish-sunkara <satishsunkara01@gmail.com>
2023-12-19 19:38:12 +0530

    logfiles delrtion

---
+++ mail.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TEAM=$1
+ALERT_TYPE=$2
+BODY=$3
+ESCAPED_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
+TO_SEND=$4
+SUB=$5
+
+FINAL_BODY=$(sed -e "s/Team/$TEAM/g" -e "s/ALERT-TYPE/$ALERT_TYPE/g" -e "s/BODY/$BODY/g" templet.html)
+
+echo "$FINAL_BODY" | mail -s "$SUB" "$TO_SEND"
--- resuce_utilization.sh
+++ resuce_utilization.sh
@@ -8,9 +8,11 @@ while IFS= read line
 do 
         usage=$(echo $line | awk '{print $6}'|cut -d % -f1 )
         partition=$(echo $line | awk '{print $1}')
-        message+="Max reahed disks are : $partition : $usage \n"
+        message+="Max reahed disks are : $partition : $usage <br>"
 done <<< $DISK_USAGE
 
 echo -e "message : $message"
 
 echo -e "$message" | mail -s "High Disk Utilization" prasannapaila99@gmail.com
+
+sh mail.sh "Devops Team" "Over Utilization of Disk" "$message" "prasannapaila99@gmail.com" "ALERT: Heavy Usage of Disks"
