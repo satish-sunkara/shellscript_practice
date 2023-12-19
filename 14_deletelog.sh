68a18bbbf3fdfbab1c31e35c27bdcb1806e16dc8
satish-sunkara <satishsunkara01@gmail.com>
2023-12-19 14:49:39 +0530

    logfiles delrtion

--- 14_deletelog.sh
+++ 14_deletelog.sh
@@ -2,13 +2,17 @@
 
 LOGFOLDER=$1
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 if [ ! -d $LOGFOLDER ]; then
-    echo " $R There is no directory $LOGFOLDER is prasenr $N "
+    echo " $R There is no directory $LOGFOLDER is prasent $N "
     exit 1
 fi
-# LOGFILE=$( $logfolder | find $1 -type f -mtime +14 -name "*.log")
+LOGFILE=$( cd $LOGFOLDER | find $1 -type f -mtime +14 -name "*.log")
 
-# while IFS= read -r line
-# do
-#     echo "oLD log files are :$LOGFILE"
-# done < LOGFILE
+while IFS= read -r line
+do
+    echo "oLD log files are :$LOGFILE"
+done < LOGFILE
