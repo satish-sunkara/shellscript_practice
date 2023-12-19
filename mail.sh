8ddbbca7d73d4c2e2ff730c1af1cc29a22b338ce
satish-sunkara <satishsunkara01@gmail.com>
2023-12-19 19:50:46 +0530

    logfiles delrtion

--- mail.sh
+++ mail.sh
@@ -9,4 +9,4 @@ SUB=$5
 
 FINAL_BODY=$(sed -e "s/Team/$TEAM/g" -e "s/ALERT-TYPE/$ALERT_TYPE/g" -e "s/BODY/$ESCAPED_BODY/g" templet.html)
 
-echo "$FINAL_BODY" | mail -s "$SUB" "$TO_SEND"
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUB\nContent-Type: text/html")" "$TO_SEND"
