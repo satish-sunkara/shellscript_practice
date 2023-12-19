b149f704d6e148936bde9fa4efcb53de92da198d
satish-sunkara <satishsunkara01@gmail.com>
2023-12-19 19:38:55 +0530

    logfiles delrtion

--- mail.sh
+++ mail.sh
@@ -7,6 +7,6 @@ ESCAPED_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
 TO_SEND=$4
 SUB=$5
 
-FINAL_BODY=$(sed -e "s/Team/$TEAM/g" -e "s/ALERT-TYPE/$ALERT_TYPE/g" -e "s/BODY/$BODY/g" templet.html)
+FINAL_BODY=$(sed -e "s/Team/$TEAM/g" -e "s/ALERT-TYPE/$ALERT_TYPE/g" -e "s/BODY/$ESCAPED_BODY/g" templet.html)
 
 echo "$FINAL_BODY" | mail -s "$SUB" "$TO_SEND"
