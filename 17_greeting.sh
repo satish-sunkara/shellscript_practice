bab4ddd24169085fca76c34f16da721d1f69cb7e
satish-sunkara <satishsunkara01@gmail.com>
2023-12-20 11:24:59 +0530

    logfiles delrtion

---
+++ 17_greeting.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+NAME=""
+WISHES=""
+
+USAGE(){
+
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options ::"
+    echo "-n , Specify the name(Mandatory)"
+    echo "-w , Specify the Wishes(Mandatory)"
+    echo "-h , Dispay help and exit"
+}
+
+
+while getopts ":n:w:h:" opt; do
+    case $opt in
+        n) NAME="$OPTARG";;
+        w) WISHES="$OPTARG";;
+        h|*) USAGE; exir;;
+    esac
+done
+
+
+echo "Hi $NAME, Very $WISHES. I am learning shell scripting"
