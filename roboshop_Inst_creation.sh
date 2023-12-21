696251fee6a16cbe2deef7f1a6925169f67451dd
satish-sunkara <satishsunkara01@gmail.com>
2023-12-21 14:28:31 +0530

    logfiles delrtion

--- roboshop_Inst_creation.sh
+++ roboshop_Inst_creation.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-03265a0778a880afb"
 SG_ID="sg-06069d2b0bdaf0215"
+HOST_ZONEID=Z08239781GWZF21X2KM8L
+DOMAIN=saachi.online
 INSTANCE_LIST=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "paymenat" "dispatch" "web")
 
 for i in "${INSTANCE_LIST[@]}"; do
@@ -12,5 +14,26 @@ for i in "${INSTANCE_LIST[@]}"; do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id $AMI_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    IP_ADDR=$(aws ec2 run-instances --image-id $AMI_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+
+    echo "$i :: $IP_ADDR"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $HOST_ZONEID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "CREATE"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$I'.'$DOMAIN'"
+            ,"Type"             : "CNAME"
+            ,"TTL"              : 120
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDR'"
+            }]
+        }
+        }]
+    }
+        '
 done
