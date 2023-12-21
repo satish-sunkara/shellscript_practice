b67cd5f67674aff73ea637610d6608c71b1e2987
satish-sunkara <satishsunkara01@gmail.com>
2023-12-21 13:24:37 +0530

    logfiles delrtion

--- roboshop_Inst_creation.sh
+++ roboshop_Inst_creation.sh
@@ -12,5 +12,5 @@ for i in "${INSTANCE_LIST[@]}"; do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id $AMI_ID  --instance-type $INSTANCE_TYPE  --security-group-ids SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    aws ec2 run-instances --image-id $AMI_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
