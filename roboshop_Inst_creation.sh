f20ecdba5263a1b2c229b4f8bb032d48f051c244
satish-sunkara <satishsunkara01@gmail.com>
2023-12-21 13:20:13 +0530

    logfiles delrtion

--- roboshop_Inst_creation.sh
+++ roboshop_Inst_creation.sh
@@ -4,7 +4,7 @@ AMI_ID=ami-03265a0778a880afb
 SG_ID=sg-06069d2b0bdaf0215
 INSTANCE_LIST=("mongodb","redis","mysql","rabbitmq","catalogue","cart","user","shipping","paymenat","dispatch","web")
 
-for i in $INSTANCE_LIST; do
+for i in "${INSTANCE_LIST[@]}"; do
     echo "Instance is : $i"
     if [ $i == "mongodb" ] || [ $i == "shipping" ] || [ $i == "mysql" ]; then
         INSTANCE_TYPE="t3.small"
